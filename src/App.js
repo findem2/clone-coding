4739f614c423facba4df72ed06f361e355824174
findem2 <thsalsqhr@gmail.com>
2024-08-19 15:30:06 +0900

    feat:somthing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import
 
 function App() {
   return (
     <div className="App">
-      <header >
-      FREE US GROUND SHIPPING ON ORDERS OF $99 OR MORE. FREE GROUND SHIPPING IN CANADA ON ORDERS OF $250 OR MORE
+      <header>
+        FREE US GROUND SHIPPING ON ORDERS OF $99 OR MORE. FREE GROUND SHIPPING
+        IN CANADA ON ORDERS OF $250 OR MORE
       </header>
-      <body>
-      </body>
-      <footer>
-      </footer>
+      <body></body>
+      <footer></footer>
     </div>
   );
 }
