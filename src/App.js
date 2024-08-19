65c735e324bc33b9ae2992071d79a4df29934b10
findem2 <thsalsqhr@gmail.com>
2024-08-19 15:28:28 +0900

    feat:somthing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header >
+      FREE US GROUND SHIPPING ON ORDERS OF $99 OR MORE. FREE GROUND SHIPPING IN CANADA ON ORDERS OF $250 OR MORE
       </header>
+      <body>
+      </body>
+      <footer>
+      </footer>
     </div>
   );
 }
